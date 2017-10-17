ac797eb6cae5696942f7523ba7ca2dcc067e00e8
Evgenia S <sdevgenia@gmail.com>
2017-10-17 21:08:16 +0300

    Add Lab3.py

---
+++ Lab3.py
@@ -0,0 +1,34 @@
+import urllib.request
+import re
+import html2text
+import pandas as pd
+
+site = urllib.request.urlopen('https://www.accuweather.com/ru/world-weather')
+html = site.read().decode('utf8')
+
+city = re.findall(r'<a href="https://www.accuweather.com/ru/.*/\d+">[A-Za-z]+</a>', html)
+temperature = re.findall(r'<span>(\d+).*</span>', html)
+
+h = html2text.HTML2Text()
+h.ignore_links = True
+
+i = 0
+cityArray = []
+temperatureArray = []
+for item in city:
+    cityArray.append(h.handle(item))
+    temperatureArray.append(h.handle(temperature[i]))
+    i += 1
+
+ca = []
+ta = []
+for i in cityArray:
+    ca.append(i.rstrip())
+for i in temperatureArray:
+    ta.append(i.rstrip())
+
+print(ca, "\n", ta)
+
+result = pd.DataFrame(data={'City:': ca, 'Temperature:': ta})
+result.to_csv('weather.csv', index=False)
+
